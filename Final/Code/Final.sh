dbd4d7bc68d41df072d99789c73dba497e74554d
1995mazdamiata <nickstorti52@gmail.com>
2026-08-12 21:23:30 -0500

    new outputs

--- Final/Code/Final.sh
+++ Final/Code/Final.sh
@@ -2,24 +2,24 @@
 
 echo $HOSTNAME
 
-/data/stortin/code/ELC5319/Final/Final 16 1
-/data/stortin/code/ELC5319/Final/Final 64 1
-/data/stortin/code/ELC5319/Final/Final 200 1
-/data/stortin/code/ELC5319/Final/Final 500 1
-/data/stortin/code/ELC5319/Final/Final 1000 1
-/data/stortin/code/ELC5319/Final/Final 2000 1
-/data/stortin/code/ELC5319/Final/Final 5000 1
-/data/stortin/code/ELC5319/Final/Final 16 2
-/data/stortin/code/ELC5319/Final/Final 64 2
-/data/stortin/code/ELC5319/Final/Final 200 2
-/data/stortin/code/ELC5319/Final/Final 500 2
-/data/stortin/code/ELC5319/Final/Final 1000 2
-/data/stortin/code/ELC5319/Final/Final 2000 2
-/data/stortin/code/ELC5319/Final/Final 5000 2
-/data/stortin/code/ELC5319/Final/Final 16 3
-/data/stortin/code/ELC5319/Final/Final 64 3
-/data/stortin/code/ELC5319/Final/Final 200 3
-/data/stortin/code/ELC5319/Final/Final 500 3
-/data/stortin/code/ELC5319/Final/Final 1000 3
-/data/stortin/code/ELC5319/Final/Final 2000 3
-/data/stortin/code/ELC5319/Final/Final 5000 3
+/data/stortin/code/ELC5319/Final/Code/Final 16 1
+/data/stortin/code/ELC5319/Final/Code/Final 64 1
+/data/stortin/code/ELC5319/Final/Code/Final 200 1
+/data/stortin/code/ELC5319/Final/Code/Final 500 1
+/data/stortin/code/ELC5319/Final/Code/Final 1000 1
+/data/stortin/code/ELC5319/Final/Code/Final 2000 1
+/data/stortin/code/ELC5319/Final/Code/Final 5000 1
+/data/stortin/code/ELC5319/Final/Code/Final 16 2
+/data/stortin/code/ELC5319/Final/Code/Final 64 2
+/data/stortin/code/ELC5319/Final/Code/Final 200 2
+/data/stortin/code/ELC5319/Final/Code/Final 500 2
+/data/stortin/code/ELC5319/Final/Code/Final 1000 2
+/data/stortin/code/ELC5319/Final/Code/Final 2000 2
+/data/stortin/code/ELC5319/Final/Code/Final 5000 2
+/data/stortin/code/ELC5319/Final/Code/Final 16 3
+/data/stortin/code/ELC5319/Final/Code/Final 64 3
+/data/stortin/code/ELC5319/Final/Code/Final 200 3
+/data/stortin/code/ELC5319/Final/Code/Final 500 3
+/data/stortin/code/ELC5319/Final/Code/Final 1000 3
+/data/stortin/code/ELC5319/Final/Code/Final 2000 3
+/data/stortin/code/ELC5319/Final/Code/Final 5000 3
